9307def592138493eb729c5b1072809b41949186
danicris234 <danicris234@gmail.com>
2018-08-27 11:17:21 -0500

    borramos importacion inecesaria

--- src/java/controladores/Galeria.java
+++ src/java/controladores/Galeria.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelos.Imagen;
-import modelos.Producto;
 
 /**
  *
